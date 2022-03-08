e170921966ddb98ae77387ce9687fb653f076901
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-03-08 23:41:24 +0200

    Trying some stylization on the home page..

--- cinema_app/cinema_app/landing_page/views.py
+++ cinema_app/cinema_app/landing_page/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views import View
+
+from cinema_app.movies.models import Movie
 
 
-class ShowLandingPage(TemplateView):
-    template_name = 'landing_page.html'
+class ShowLandingPage(View):
+    def get(self, request):
+        "TODO filter top 3 liked movies"
+        movies = Movie.objects.all()[:3]
+        context = {
+            'movies': movies,
+        }
+
+        return render(request, 'landing_page.html', context)
