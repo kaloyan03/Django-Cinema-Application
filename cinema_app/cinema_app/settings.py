185495b6d2d5b4f4426f22e86a1dae9bb6fff9aa
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-03 23:41:15 +0300

    Freeze in requirements

--- cinema_app/cinema_app/settings.py
+++ cinema_app/cinema_app/settings.py
@@ -166,11 +166,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
