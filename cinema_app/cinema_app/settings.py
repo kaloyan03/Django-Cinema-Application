bfc13dc62087229b71ebed4a69e60599450654ec
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-03 23:18:49 +0300

    Fixed media settings

--- cinema_app/cinema_app/settings.py
+++ cinema_app/cinema_app/settings.py
@@ -169,7 +169,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
