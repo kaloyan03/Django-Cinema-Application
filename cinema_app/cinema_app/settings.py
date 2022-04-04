d5c276e4209b1920b279881c6ea94111dc8847e3
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-04 16:00:24 +0300

    Debug made True for testing

--- cinema_app/cinema_app/settings.py
+++ cinema_app/cinema_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 """
 'False' == 'True' => False
