7b665813e3fa0720c06bbb957b9a579b1a14c0af
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-05 18:27:49 +0300

    Commit for test purpose only

--- cinema_app/cinema_app/settings.py
+++ cinema_app/cinema_app/settings.py
@@ -198,3 +198,5 @@ cloudinary.config(
     api_key=os.getenv('CLOUDINARY_API_KEY', None),
     api_secret=os.getenv('CLOUDINARY_API_SECRET', None)
 )
+
+print(os.getenv('CLOUDINARY_CLOUD_NAME', None),)
