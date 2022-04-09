cada70bc352339e73bbd61c9ae6b58afd8f8dbf4
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-09 11:46:04 +0300

    Written tests for tickets_cart app, only for the CartView view

--- cinema_app/cinema_app/tickets_cart/tests/test_views.py
+++ cinema_app/cinema_app/tickets_cart/tests/test_views.py
@@ -1,21 +1,74 @@
-from cart.cart import Cart
 from django import test as django_test
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
+from django.core import mail
+
+from cinema_app.movies.models import Movie, Ticket
+from cinema_app.tickets_cart.models import Cart, Item
+
+VALID_MOVIE_DATA = {
+    'title': 'Avatar',
+    'image': SimpleUploadedFile(name='test_image.jpg',
+                                content=open(r"C:\Users\PC\Pictures\Softuni Cinema Project Photos\spider-man.jpg",
+                                             'rb').read(), content_type='image/jpeg'),
+    'trailer_video': 'https://www.youtube.com/watch?v=zAH-GjT4Jpw',
+    'genre': 'Adventure',
+    'year': 2010,
+    'description': 'Ben-10 The Movie.',
+    'duration': 140,
+    'category': 'B',
+}
 
 UserModel = get_user_model()
 
 class CartViewTests(django_test.TestCase):
-    VALID_USER_CREDENTIALS = {
+    USER_CREDENTIALS = {
         'email': 'testtest@abv.bg',
-        'password': '123313123231qwe',
+        'password': '1312231321qwe',
     }
 
     def setUp(self) -> None:
-        UserModel.objects.create_user(**self.VALID_USER_CREDENTIALS)
-        self.client.login(**self.VALID_USER_CREDENTIALS)
+        movie = Movie.objects.create(**VALID_MOVIE_DATA)
+        Ticket.objects.create(movie=movie, price=15)
 
-    def test_get__correct_template_given__expect_correct_template(self):
-        response = self.client.get(reverse('list movies'))
+        UserModel.objects.create_user(**self.USER_CREDENTIALS)
+        self.client.login(**self.USER_CREDENTIALS)
 
-        self.assertTemplateUsed(response, 'checkout_cart.html')
+
+    def test_get__context__expect_to_have_tickets_total_price_total_quantity(self):
+        user = UserModel.objects.first()
+        ticket = Ticket.objects.first()
+        cart = Cart.objects.get(user=user)
+        Item.objects.create(ticket=ticket, cart=cart, quantity=1)
+
+
+
+        response = self.client.get(reverse('cart view'))
+        ticket = response.context['tickets'][0]
+        total_price = response.context['total_price']
+        total_quantity = response.context['total_quantity']
+
+        self.assertIsNotNone(ticket)
+        # test ticket attributes that are added
+        self.assertIsNotNone(ticket.quantity)
+        self.assertIsNotNone(ticket.total_price)
+        self.assertIsNotNone(ticket.item_id)
+        # end
+
+        self.assertIsNotNone(total_price)
+        self.assertIsNotNone(total_quantity)
+
+    def test_post__expect_to_delete_cart_and_send_mail(self):
+        user = UserModel.objects.first()
+        ticket = Ticket.objects.first()
+        cart = Cart.objects.get(user=user)
+        Item.objects.create(ticket=ticket, cart=cart, quantity=1)
+
+
+
+        self.client.post(reverse('cart view'))
+
+        cart = Cart.objects.first()
+        self.assertIsNone(cart)
+        self.assertEqual(len(mail.outbox), 1)
