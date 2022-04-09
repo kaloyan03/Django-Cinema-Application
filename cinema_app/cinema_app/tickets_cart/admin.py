95f71e5d7b2c3a6e4b98b8b18edc8652703edc7e
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-09 22:57:46 +0300

    Added admin for all modles

--- cinema_app/cinema_app/tickets_cart/admin.py
+++ cinema_app/cinema_app/tickets_cart/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from cinema_app.tickets_cart.models import Cart, Item
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user']
+    filter_horizontal = ['tickets']
+
+
+admin.site.register(Cart, CartAdmin)
+
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['quantity', 'ticket']
+
+
+admin.site.register(Item, ItemAdmin)
