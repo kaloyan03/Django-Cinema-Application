c10c4b8f64d6233c64a23c49d5aae54e36990ba1
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-13 23:22:06 +0300

    Fixed tests for tickets_cart app

--- cinema_app/cinema_app/tickets_cart/tests/test_views.py
+++ cinema_app/cinema_app/tickets_cart/tests/test_views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.core import mail
 
 from cinema_app.movies.models import Movie, Ticket
+from cinema_app.projections.models import Projection
 from cinema_app.tickets_cart.models import Cart, Item
 
 VALID_MOVIE_DATA = {
@@ -20,29 +21,35 @@ VALID_MOVIE_DATA = {
     'category': 'B',
 }
 
+VALID_PROJECTION_DATA = {
+    'day_of_the_week': 'Monday',
+    'time': '18:00',
+}
+
 USER_CREDENTIALS = {
-        'email': 'testtest@abv.bg',
-        'password': '1312231321qwe',
-    }
+    'email': 'testtest@abv.bg',
+    'password': '1312231321qwe',
+}
 
 UserModel = get_user_model()
+
 
 class CartViewTests(django_test.TestCase):
     def setUp(self) -> None:
         movie = Movie.objects.create(**VALID_MOVIE_DATA)
-        Ticket.objects.create(movie=movie, price=15)
-
+        ticket = Ticket.objects.create(movie=movie, price=15)
+        projection = Projection.objects.create(day_of_the_week=VALID_PROJECTION_DATA['day_of_the_week'],
+                                               time=VALID_PROJECTION_DATA['time'])
+        projection.ticket.add(ticket)
         UserModel.objects.create_user(**USER_CREDENTIALS)
         self.client.login(**USER_CREDENTIALS)
-
 
     def test_get__context__expect_to_have_tickets_total_price_total_quantity(self):
         user = UserModel.objects.first()
         ticket = Ticket.objects.first()
         cart = Cart.objects.get(user=user)
-        Item.objects.create(ticket=ticket, cart=cart, quantity=1)
-
-
+        projection = Projection.objects.first()
+        Item.objects.create(ticket=ticket, cart=cart, quantity=1, projection=projection)
 
         response = self.client.get(reverse('cart view'))
         ticket = response.context['tickets'][0]
@@ -59,31 +66,35 @@ class CartViewTests(django_test.TestCase):
         self.assertIsNotNone(total_price)
         self.assertIsNotNone(total_quantity)
 
-    def test_post__expect_to_delete_cart_and_send_mail(self):
+    def test_post__expect_to_delete_item_and_send_mail(self):
         user = UserModel.objects.first()
         ticket = Ticket.objects.first()
         cart = Cart.objects.get(user=user)
-        Item.objects.create(ticket=ticket, cart=cart, quantity=1)
+        projection = Projection.objects.first()
+        Item.objects.create(ticket=ticket, cart=cart, quantity=1, projection=projection)
 
         self.client.post(reverse('cart view'))
 
-        cart = Cart.objects.first()
-        self.assertIsNone(cart)
+        item = Item.objects.first()
+        self.assertIsNone(item)
         self.assertEqual(len(mail.outbox), 1)
 
 
 class AddToCartTests(django_test.TestCase):
     def setUp(self) -> None:
         movie = Movie.objects.create(**VALID_MOVIE_DATA)
-        Ticket.objects.create(movie=movie, price=15)
-
+        ticket = Ticket.objects.create(movie=movie, price=15)
+        projection = Projection.objects.create(day_of_the_week=VALID_PROJECTION_DATA['day_of_the_week'],
+                                               time=VALID_PROJECTION_DATA['time'])
+        projection.ticket.add(ticket)
         UserModel.objects.create_user(**USER_CREDENTIALS)
         self.client.login(**USER_CREDENTIALS)
 
     def test_post__add_ticket_to_cart__expect_ticket_to_be_added(self):
         self.client.post(reverse('add to cart', kwargs={
-            'id': Movie.objects.first().id,
+            'ticket_id': Ticket.objects.first().id,
             'quantity': 1,
+            'projection_id': Projection.objects.first().id,
         }))
 
         cart = Cart.objects.first()
@@ -95,15 +106,18 @@ class AddToCartTests(django_test.TestCase):
 class RemoveFromCartTests(django_test.TestCase):
     def setUp(self) -> None:
         movie = Movie.objects.create(**VALID_MOVIE_DATA)
-        Ticket.objects.create(movie=movie, price=15)
-
+        ticket = Ticket.objects.create(movie=movie, price=15)
+        projection = Projection.objects.create(day_of_the_week=VALID_PROJECTION_DATA['day_of_the_week'],
+                                               time=VALID_PROJECTION_DATA['time'])
+        projection.ticket.add(ticket)
         UserModel.objects.create_user(**USER_CREDENTIALS)
         self.client.login(**USER_CREDENTIALS)
 
     def test_post__remove_ticket_from_cart__expect_ticket_to_be_removed(self):
         self.client.post(reverse('add to cart', kwargs={
-            'id': Movie.objects.first().id,
+            'ticket_id': Ticket.objects.first().id,
             'quantity': 1,
+            'projection_id': Projection.objects.first().id,
         }))
 
         item = Cart.objects.first().item_set.first()
@@ -116,4 +130,3 @@ class RemoveFromCartTests(django_test.TestCase):
         items_in_cart = cart.item_set.all()
 
         self.assertEqual(items_in_cart[0].quantity, 0)
-
